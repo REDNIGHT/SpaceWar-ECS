385dde28ddfe04c09b0389491b9d84d80f86f12b
chxl <chxlchoosemylife@126.com>
2020-01-28 02:43:06 +0800

    添加音效...

---
+++ Assets/RNKit/ECS/_SpaceWar/PhysicsTrigger/OnTriggerWithoutInstalledWeapon.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace RN.Network.SpaceWar
+{
+    public class OnTriggerWithoutInstalledWeapon : OnTrigger
+    {
+        protected override void onTrigger(Collider other, TriggerResultState state)
+        {
+            if (entityManager == null) return;
+            if (entityManager.Exists(entity) == false) return;
+
+
+            if (EntityBehaviour.getEntity(other.attachedRigidbody, out Entity transformEntity, entityManager.World))
+            {
+                if (entityManager.HasComponent<WeaponInstalledState>(transformEntity))
+                {
+                    return;
+                }
+
+                entityManager.GetBuffer<PhysicsTriggerResults>(entity).Add(new PhysicsTriggerResults { state = state, entity = transformEntity });
+            }
+            else
+            {
+                var entityBehaviour = other.GetComponent<EntityBehaviour>();
+                if (entityBehaviour == null) return;
+
+                //别的world里的Collider 进入到这Trigger
+                if (world != entityBehaviour.world) return;
+
+#if UNITY_EDITOR
+                var entityName = entityManager.GetName(entityBehaviour.entity);
+#else
+                var entityName = "";
+#endif
+                Debug.LogError($"EntityBehaviour.getEntity(other={other}, ...) == false" +
+                    $"\nentityBehaviour.entity={entityBehaviour.entity}  {entityName}  Exists={entityManager.Exists(entityBehaviour.entity)}" +
+                    $"\nentityBehaviour.world={entityBehaviour.world}  entityManager.World={entityManager.World}" +
+                    $"\nthis={this}"
+                    , other);
+            }
+        }
+    }
+}
---
+++ Assets/_spaceWar/scripts/fx/MouseFx.cs
@@ -0,0 +1,25 @@
+﻿
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace RN.Network.SpaceWar
+{
+    public class MouseFx : MonoBehaviour, IMouseFx
+    {
+        ParticleSystem ps;
+        AudioSource @as;
+        private void Awake()
+        {
+            ps = GetComponent<ParticleSystem>();
+            @as = GetComponent<AudioSource>();
+        }
+        public void OnPlayFx(in float3 position)
+        {
+            ps.transform.position = position;
+            ps.Emit(1);
+            ps.gameObject.SetActive(true);
+
+            @as.Play();
+        }
+    }
+}
